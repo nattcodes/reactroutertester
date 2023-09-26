0f4d180fcc7b7c682e9022c8f7622b331d60a711
Kome Nathaniel <77756722+nattcodes@users.noreply.github.com>
2023-09-26 01:28:42 +0100

    h

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 
 //pages
-import Home from "./Pages/Home";
-import About from "./Pages/About";
-import Faq from "./Pages/Faq";
-import Contact from "./Pages/Contact";
-import Careers, { careerLoader } from "./Pages/Careers";
-import CareerDetails, { careerDetailsParameter } from "./Pages/CareerDetails";
+import Home from "Pages/Home";
+import About from "Pages/About";
+import Faq from "Pages/Faq";
+import Contact from "Pages/Contact";
+import Careers, { careerLoader } from "Pages/Careers";
+import CareerDetails, { careerDetailsParameter } from "Pages/CareerDetails";
+import CatchCareerError from "Pages/CatchCareerError";
 
 //layout
-import RootLayout from "./Layout/RootLayout";
-import Help from "./Layout/Help";
-import CareerLayout from "./Layout/CareerLayout";
+import RootLayout from "Layout/RootLayout";
+import Help from "Layout/Help";
+import CareerLayout from "Layout/CareerLayout";
 
 //Router
 import { 
@@ -20,7 +21,6 @@ import {
   Route,
   RouterProvider 
 } from "react-router-dom";
-import CatchCareerError from "./Pages/CatchCareerError";
 
 
 
